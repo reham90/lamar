03c75cdfc00d760e84de59feffa84dfc5fa4e4a3
reham90 <rehamadel344@gmail.com>
2021-12-15 18:32:26 +0300

    edit

--- js/my-js.js
+++ js/my-js.js
@@ -3,92 +3,22 @@ $(document).ready(function() {
 
     // start js for navbar //
 
-    (function($) { // Begin jQuery36.
-        $(function() { // DOM ready
-            // If a link has a dropdown, add sub menu toggle.
-            $('.menu-link').click(function(e) {
-                $('.menu-link').not($(this)).removeClass("active");
-                $(this).toggleClass("active");
-                $(this).siblings('.nav-dropdown').slideToggle(200);
+    $(".menu-button").click(function() {
+        $(".menu-screen").addClass("active");
+    });
 
+    // Remove Active Class
+    $(".close-button").click(function() {
+        $(".menu-screen").removeClass("active");
+    });
 
-                // Close one dropdown when selecting another
-                $('.nav-dropdown').not($(this).siblings()).hide();
-
-
-                e.stopPropagation();
-            });
-            // Clicking away from dropdown will remove the dropdown class
-
-
-
-
-            $('.re-dropdown').click(function(e) {
-                $('.menu-link').not($(this)).removeClass("active");
-                $('.drop-mobile').toggle();
-                $('.nav-dropdown').hide();
-
-
-
-
-                // Close one dropdown when selecting another
-            });
-
-
-            // Toggle open and close nav styles on click
-            $('.nav-toggle').click(function() {
-
-                $(".re-hide").css("display", "block");
-                $(".cover").toggle();
-            });
-            $('.re-show').click(function(event) {
-                if (document.dir == "rtl") {
-                    $('.nav-nav ').css("right", "0");
-                } else {
-                    $('.nav-nav ').css("left", "0");
-
-                }
-                $("body").css("overflow", "hidden");
-                $(".navv").toggle();
-                if ($('.nav-list').hasClass('dismiss')) {
-                    $('.nav-list').removeClass('dismiss').addClass('selected').show();
-                }
-                event.preventDefault();
-            });
-            $('.re-hide').click(function(event) {
-                if ($('.nav-list').hasClass('selected')) {
-                    $('.nav-list').removeClass('selected').addClass('dismiss');
-                }
-                event.preventDefault();
-                $("body").css("overflow", "auto");
-                $(".cover").toggle();
-
-            });
-            $('.cover').on('click', function() {
-                $(".re-hide").click();
-            });
-
-        }); // end DOM ready
-    })(jQuery); // end jQuery83"9?"
 
     // end  js for navbar //
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // start search pop up //
 
-    $(".search-button").click(function() {
-        $(".search-screen").addClass("active");
-    });
-
-    // Remove Active Class
-    $(".close-button").click(function() {
-        $(".search-screen").removeClass("active");
-    });
-
-
-    // end search pop up //
 
     // start footer accordion  //
     if ($(window).width() < 991) {
@@ -113,7 +43,61 @@ $(document).ready(function() {
     //end footer accordion //
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // start tabs js //
 
+    // tabs
+
+    var tabLinks = document.querySelectorAll(".tablinks");
+    var tabContent = document.querySelectorAll(".tabcontent");
+
+
+    tabLinks.forEach(function(el) {
+        el.addEventListener("click", openTabs);
+    });
+
+
+    function openTabs(el) {
+        var btnTarget = el.currentTarget;
+        var country = btnTarget.dataset.country;
+
+        tabContent.forEach(function(el) {
+            el.classList.remove("active");
+        });
+
+        tabLinks.forEach(function(el) {
+            el.classList.remove("active");
+        });
+
+        document.querySelector("#" + country).classList.add("active");
+
+        btnTarget.classList.add("active");
+    }
+
+
+    // end tabs js //
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // start scroll to top button //
+
+    var btn = $('.myBtn');
+
+    $(window).scroll(function() {
+
+    });
+
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+
+
+    // end scroll to top button //
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     // start slider carousel //
@@ -146,78 +130,47 @@ $(document).ready(function() {
 
     // end slider carousel //
     ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // product carousel //
+    // start new carousel //
+    if ($(window).width() < 991) {
 
-    $('.product').owlCarousel({
+        $('.new').owlCarousel({
 
-        nav: true,
-        rtl: document.dir == 'rtl' ? true : false,
-        loop: true,
+            nav: true,
+            rtl: document.dir == 'rtl' ? true : false,
+            loop: true,
+            autoplay: true,
+            autoplayTimeout: 3000,
+            autoplayHoverPause: true,
+            navText: ["<span class='lnr lnr-chevron-right right-arrow'></span>", "<span class='lnr lnr-chevron-left left-arrow'></span>"],
+            responsive: {
+                0: {
 
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        navText: ["<span class='lnr lnr-chevron-right right-arrow'></span>", "<span class='lnr lnr-chevron-left left-arrow'></span>"],
-        responsive: {
-            0: {
-                autoplay: true,
-                margin: 15,
-                items: 2
-            },
-            600: {
-                autoplay: true,
-                margin: 30,
-                items: 2
-            },
-            1200: {
-                autoplay: false,
-                margin: 30,
-                items: 4
+                    margin: 15,
+                    items: 1
+                },
+                600: {
+
+                    margin: 30,
+                    items: 2
+                },
+                1200: {
+
+                    margin: 30,
+                    items: 4
+                }
+
             }
+        });
+    };
 
-        }
-    })
-
-    // end product carousel //
+    // end new carousel //
 
 
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // feature carousel //
 
-    $('.feature').owlCarousel({
-
-        nav: true,
-        rtl: document.dir == 'rtl' ? true : false,
-        loop: true,
-
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        navText: ["<span class='lnr lnr-chevron-right right-arrow'></span>", "<span class='lnr lnr-chevron-left left-arrow'></span>"],
-        responsive: {
-            0: {
-                autoplay: true,
-                margin: 15,
-                items: 2
-            },
-            600: {
-                autoplay: false,
-                margin: 30,
-                items: 2
-            },
-            1200: {
-                autoplay: false,
-                margin: 94,
-                items: 4
-            }
-
-        }
-    })
-
-    // end feature carousel //
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     /// wow js ///
     new WOW().init();
